71bdea8501210a6b4bf015243f33b99c543dc18d
johnjohnn <conde2010@aol.com>
2021-09-14 23:23:28 -0400

    fixing api request

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+
+photosArray= [];
+
+
 // Unsplas API      
 const count = 10;
 const apiKey="kbIZCdoL6eLiQd5PDkIqEAOs1q0omT94fUwRt-sn8Wk"    
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Create Elements for Links and PHOTOS , 
+function displayPhotos() {
+    photosArray.foreach( (photo) => {
+     // Creat <a> to link to Unsplash    
+    })
+}
+
 
 // Get photos from splash API 
 
 async function getphotos(){
     try{
     const response = await fetch(apiUrl);
-    const data = await response.json();
-    console.log(data);
+    photosArray = await response.json();
+    displayPhotos();
 
     }catch (error){
 //catch error here
