4b01580b69aec36b4fd2951976501fc5be841286
johnjohnn <conde2010@aol.com>
2021-09-16 17:51:12 -0400

    adding the infinite scroll

--- script.js
+++ script.js
@@ -57,6 +57,12 @@ async function getphotos(){
 //catch error here
     }
 }
-
+// Check to see if scrolling near bottom of page , Load More Photos 
+window.addEventListener("scroll", () => {
+    if (window.innerHeight  + window.scrollY >= document.body.offsetHeight - 1000){
+        getphotos()
+    }
+}
+ )
 // on load 
 getphotos()
